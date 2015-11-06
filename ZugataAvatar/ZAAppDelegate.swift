2344f69dc957a2ceaf21abcf0cdb06df183a9f08
Kevin Xu <kevin@imkevinxu.com>
2015-11-05 19:39:37 -0800

    [Enhancement] Prefixed starter files with ZA

---
+++ ZugataAvatar/ZAAppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  ZAAppDelegate.swift
+//  ZugataAvatar
+//
+//  Created by Kevin Xu on 11/5/15.
+//  Copyright © 2015 Kevin Xu. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Properties
+
+@UIApplicationMain
+class ZAAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
+// MARK: - Application Lifecycle
+
+extension ZAAppDelegate {
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
+        return true
+    }
+}
---
+++ ZugataAvatar/ZAViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ZAViewController.swift
+//  ZugataAvatar
+//
+//  Created by Kevin Xu on 11/5/15.
+//  Copyright © 2015 Kevin Xu. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Properties
+
+class ZAViewController: UIViewController {
+
+}
+
+// MARK: - View Lifecycle
+
+extension ZAViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
